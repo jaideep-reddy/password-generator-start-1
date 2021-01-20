42454d155fe24c7ff0f04e0e23eba7348217c749
jaideepreddy <opgamergamesalot@gmail.com>
2021-01-20 14:22:16 +0000

    i made a genrator

--- main.py
+++ main.py
@@ -8,10 +8,45 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+print(input("☠️ DO NOT ADD A LONG NUMBER IT MIGHT CRASH YOUR PC WARNING ☠️ press ENTER to continue\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# password = ""
 
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#  password += random.choice(numbers)
+
+# print(f'Your password could be: "{password}"')
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+ password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+ password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f'your pasword cold be: "{password}"')
+
+
+
+
+
